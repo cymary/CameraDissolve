b9a7d9ca84d3ef4c3e575c466832f94274fb1fa1
cymary <36532367+cymary@users.noreply.github.com>
2023-08-11 01:35:16 -0700

    Update CameraDissolve01.py
    + add two retime camera
    + maybe need node Class checking see if it's camera

--- CameraDissolve01.py
+++ CameraDissolve01.py
@@ -1,11 +1,18 @@
-#this works on the nuke v014.0v3 best
-# need to color the camera to red
-#camera in ord version is camera 2 mayneed switch
-# neeed to record the turotial , best with camera projection demonstrate
 
 def CameraDissolve():
+
     # select nodes
     sNodes = nuke.selectedNodes()
+
+
+    ## check error
+
+    # if select No nodes
+    if len(sNodes)!=2:
+        nuke.message('Please select only two Camera')
+        return()
+            
+
 
     # Get Camera
     aCamNode = sNodes[-1]
@@ -25,51 +32,86 @@ def CameraDissolve():
 
     # Create new Camera
     nCamera = nuke.createNode('Camera3')
-    nCamera['tile_color'].setValue(0x7f00ffff)
+    nCamera['tile_color'].setValue(0xff00bfff)
+    nCamera['gl_color'].setValue(0xff00bfff)
     nCamera['label'].setValue('CameraDissolve')
 
-
-
-    #Prepare Reference Camera Python expression content 
-
-    deselectAllNodes = '[n.knob("selected").setValue(False) for n in nuke.allNodes()]'
-    
-    pySelectACam = 'nuke.toNode(\'%s\')["selected"].setValue(1)'%aCamName
-    pySelectBCam = 'nuke.toNode(\'%s\')["selected"].setValue(1)'%bCamName
-    
-    
-    
-    
-    
-    pyCopyCamToMem = 'nuke.nodeCopy("%clipboard%")'
-    pypasteRetimeCam = 'RetimeCam = nuke.nodePaste("%clipboard%")'
-
-    pyDisconnectAll = 'RetimeCam.setInput(0, None)'
-    pyLabel = 'RetimeCam["label"].setValue("Retimed")'
-    
-    #combine all the python code inside the script botton
-    
-    aCamRetimePyContent = '{}{}{}{}{}{}{}{}{}{}{}'.format(deselectAllNodes,'\n',pySelectACam,'\n',pyCopyCamToMem,'\n',pypasteRetimeCam,'\n',pyDisconnectAll,'\n',pyLabel)
-    bCamRetimePyContent = '{}{}{}{}{}{}{}{}{}{}{}'.format(deselectAllNodes,'\n',pySelectBCam,'\n',pyCopyCamToMem,'\n',pypasteRetimeCam,'\n',pyDisconnectAll,'\n',pyLabel)
-
+    # Deselected All nodes
+    for n in nuke.allNodes():
+        n.knob("selected").setValue(False) 
 
     
+    # Create Retimed A Cam
+
+    refACam = nuke.createNode('Camera3')
+    refACam['tile_color'].setValue(int(aCamNode['tile_color'].getValue()))
+    refACam['gl_color'].setValue(int(aCamNode['tile_color'].getValue()))
+    refACam['label'].setValue('Retimed A Cam') 
+
+    # Deselected All nodes
+    for n in nuke.allNodes():
+        n.knob("selected").setValue(False) 
+
+    
+    # Create Retimed B Cam
+    refBCam = nuke.createNode('Camera3')
+    refBCam['tile_color'].setValue(int(bCamNode['tile_color'].getValue()))
+    refBCam['gl_color'].setValue(int(bCamNode['tile_color'].getValue()))
+    refBCam['label'].setValue('Retimed B Cam')    
+
+    # Deselected All nodes
+    for n in nuke.allNodes():
+        n.knob("selected").setValue(False) 
+
+    
+
     # Create custom CamDissove tag and knobs 
+
     camMixSliderTab = nuke.Tab_Knob('cameraDissolveTab', 'CamDissolve')
     aCamTimeOffsetknob = nuke.Int_Knob('aCameraOffset', 'A Cam Time Offset',0)
     bCamTimeOffsetknob = nuke.Int_Knob('bCameraOffset', 'B Cam Time Offset',0)   
     camMixSliderKnob = nuke.Double_Knob('camDissolve','Camera Dissolve')
-    aCamRetimed = nuke.PyScript_Knob('aCamRetimed','Create Retimed A Cam',aCamRetimePyContent)
-    bCamRetimed = nuke.PyScript_Knob('bCamRetimed','Create Retimed B Cam',bCamRetimePyContent)   
-    
-    # Collect all the knob as List
+    #aCamRetimed = nuke.PyScript_Knob('aCamRetimed','Create Retimed A Cam',aCamRetimePyContent)
+    #bCamRetimed = nuke.PyScript_Knob('bCamRetimed','Create Retimed B Cam',bCamRetimePyContent)   
 
-    panelKnobs = [camMixSliderTab,aCamTimeOffsetknob,aCamRetimed,bCamTimeOffsetknob,bCamRetimed,camMixSliderKnob]
+
+    # Collect all the knob as List for Dissolve node
+
+    panelKnobs = [camMixSliderTab,aCamTimeOffsetknob,bCamTimeOffsetknob,camMixSliderKnob]
+
+
+    #Create Retimed A Cam knobs
+    camMixSliderTab_ACam = nuke.Tab_Knob('cameraDissolveTab', 'CamDissolve')
+    aCamTimeOffsetknob_ACam = nuke.Int_Knob('aCameraOffset', 'A Cam Time Offset',0)
+    bCamTimeOffsetknob_ACam = nuke.Int_Knob('bCameraOffset', 'B Cam Time Offset',0)  
+    camMixSliderKnob_ACam = nuke.Double_Knob('camDissolve','Camera Dissolve')
+
+    # Collect a Cam knobs
+    panelKnobs_ACam = [camMixSliderTab_ACam,aCamTimeOffsetknob_ACam,bCamTimeOffsetknob_ACam,camMixSliderKnob_ACam]
+
+    #Create Retimed B Cam knobs
+    camMixSliderTab_BCam = nuke.Tab_Knob('cameraDissolveTab', 'CamDissolve')
+    aCamTimeOffsetknob_BCam = nuke.Int_Knob('aCameraOffset', 'A Cam Time Offset',0)
+    bCamTimeOffsetknob_BCam = nuke.Int_Knob('bCameraOffset', 'B Cam Time Offset',0)  
+    camMixSliderKnob_BCam = nuke.Double_Knob('camDissolve','Camera Dissolve')
+
+    # Collect a Cam knobs
+    panelKnobs_BCam = [camMixSliderTab_BCam,aCamTimeOffsetknob_BCam,bCamTimeOffsetknob_BCam,camMixSliderKnob_BCam]
+        
     
     # Add all the knobs to Panel 
     
-    for panelKnobs in panelKnobs:
-        nCamera.addKnob(panelKnobs) 
+    for panelKnob in panelKnobs:
+        nCamera.addKnob(panelKnob) 
+
+    for panelKnobRefACam in panelKnobs_ACam:
+        refACam.addKnob(panelKnobRefACam) 
+    
+
+    for panelKnobRefBCam in panelKnobs_BCam:
+        refBCam.addKnob(panelKnobRefBCam) 
+    
+
    
 
     # Get All The Knob Would Like to Blend
@@ -86,8 +128,32 @@ def CameraDissolve():
     for knob in knobsList:
         
         nCamera[knob].setExpression('lerp({}.{}{},{}.{}{},{})'.format(aCamName,knob,'(frame-aCameraOffset)',bCamName,knob,'(frame-bCameraOffset)','camDissolve'))
-        
-        
+        refACam[knob].setExpression('lerp({}.{}{},{}.{}{},{})'.format(aCamName,knob,'(frame-aCameraOffset)',bCamName,knob,'(frame-bCameraOffset)','camDissolve'))
+        refBCam[knob].setExpression('lerp({}.{}{},{}.{}{},{})'.format(aCamName,knob,'(frame-aCameraOffset)',bCamName,knob,'(frame-bCameraOffset)','camDissolve'))
+    
+    # expression linked To Dissolved Camera
+    
+
+    ## Retimed A cam Setup Value
+    # Link time offset to Dissolve Camera node
+    refACam['aCameraOffset'].setExpression('parent.%s.aCameraOffset'%nCamera.name())
+    refACam['bCameraOffset'].setExpression('parent.%s.bCameraOffset'%nCamera.name())
+    refACam['camDissolve'].setValue(0) # set value to 0 so, value match A cam 
+
+
+    refACam['aCameraOffset'].setEnabled(False)    # disable all parameter
+    refACam['bCameraOffset'].setEnabled(False)
+    refACam['camDissolve'].setEnabled(False)
+
+    ## Retimed B cam Setup Value
+    refBCam['aCameraOffset'].setExpression('parent.%s.aCameraOffset'%nCamera.name())
+    refBCam['bCameraOffset'].setExpression('parent.%s.bCameraOffset'%nCamera.name())
+    refBCam['camDissolve'].setValue(1)# set value to 1 so, value match B cam 
+
+    refBCam['aCameraOffset'].setEnabled(False)
+    refBCam['bCameraOffset'].setEnabled(False)
+    refBCam['camDissolve'].setEnabled(False)
+
 CameraDissolve()
 
 
